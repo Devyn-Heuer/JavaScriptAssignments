66b4c0fa5efec02f2debd959d1ffc10c60fce1b8
Devyn <heuers.hues@gmail.com>
2022-11-28 17:19:02 +0200

    next one

---
+++ Assignment (Conditional Statements & Loops)/answers.js
@@ -0,0 +1,19 @@
+// A
+function questionA (x, y) {
+    let biggest;
+    if(x > y){
+        biggest = x;
+    } else {
+        biggest = y;
+    }
+    console.log(biggest);
+}
+
+// B
+function questionB(num){
+    if(num % 2 == 0){
+        console.log(num + " is even.");
+    } else {
+        console.log(num + " is odd.");
+    }
+}
