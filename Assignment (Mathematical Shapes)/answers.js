4c7086f78a1bc3b25993388989a1fd778eb7f903
Devyn <heuers.hues@gmail.com>
2022-11-28 17:10:38 +0200

    one at a time

---
+++ Assignment (Mathematical Shapes)/answers.js
@@ -0,0 +1,28 @@
+// A
+function questionA() {
+    let side = 9;
+    let diagonal;
+
+    diagonal = Math.sqrt((side * side)*2)
+}
+
+// B
+function questionB() {
+    let a = 5;
+    let b = 6;
+    let c =7;
+    let triangleArea;
+
+    triangleArea = (1/4) * Math.sqrt(4 * a*a * b*b - ((a*a + b*b - c*c)**2))
+}
+
+// C
+function questionC() {
+    let radius = 4;
+    const pi = Math.PI;
+    let circumfrence;
+    let area;
+
+    circumfrence = 2 * pi * radius;
+    area = pi * radius**2;
+}
